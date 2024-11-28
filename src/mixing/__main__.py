50b6053f0b01fb80a237faeaa67e1b54e9164e26
mjoes <morten.slingsby@gmail.com>
2024-11-28 23:00:57 +0100

    refactor

--- src/mixing/__main__.py
+++ src/mixing/__main__.py
@@ -1,27 +1,33 @@
-import shutil
-import os
 import argparse
-import pandas as pd
-import gspread
-
+import os
+import shutil
 from datetime import datetime
+from pathlib import Path
+
+import gspread
+import pandas as pd
 from loguru import logger
 from pydub import AudioSegment
 from pydub.silence import detect_leading_silence
-from pathlib import Path
-from pydub import AudioSegment
-from mixing.utils import get_filename, get_metadata, add_metadata_to_mp3
 
-SHEET_ID="1j2w0MPDL0R9Z_9XE5G_luDo4PgPEWhf6RNhwbxh7lmw"
-SHEET_NAME="meta"                                                                                                                                  
+from mixing.utils import add_metadata_to_mp3, get_filename, get_metadata
 
-def trim_sound(src_audio_path, src_audio_filename, dst_audio_path, dst_audio_filename) -> str:  
+SHEET_ID = "1j2w0MPDL0R9Z_9XE5G_luDo4PgPEWhf6RNhwbxh7lmw"
+SHEET_NAME = "meta"
+
+def trim_sound(
+        src_audio_path: str, src_audio_filename: str, dst_audio_path: str, dst_audio_filename: str,
+) -> str:
     dst_path = Path(dst_audio_path) / Path(dst_audio_filename)
     src_path = Path(src_audio_path) / Path(src_audio_filename)
     try:
         trim_leading_silence: AudioSegment = lambda x: x[detect_leading_silence(x) :]
-        trim_trailing_silence: AudioSegment = lambda x: trim_leading_silence(x.reverse()).reverse()
-        strip_silence: AudioSegment = lambda x: trim_trailing_silence(trim_leading_silence(x))
+        trim_trailing_silence: AudioSegment = lambda x: trim_leading_silence(
+            x.reverse(),
+        ).reverse()
+        strip_silence: AudioSegment = lambda x: trim_trailing_silence(
+            trim_leading_silence(x),
+        )
         sound = AudioSegment.from_file(src_path)
         stripped = strip_silence(sound)
         out = stripped.fade_in(3000).fade_out(3000)
@@ -33,61 +39,68 @@ def trim_sound(src_audio_path, src_audio_filename, dst_audio_path, dst_audio_fil
     else:
         logger.info(f"PASSED: Trim and fade in/out for: {src_audio_filename}")
 
-    os.remove(src_path)
+    src_path.unlink()
 
     return str(dst_path)
 
+
 def trimming(df: pd.DataFrame, src_audio_path: str, dst_audio_path: str) -> None:
-    source_file_list = [f for f in os.listdir(src_audio_path) if not f.startswith('.')]
+    source_file_list = [f for f in os.listdir(src_audio_path) if not f.startswith(".")]
     if len(source_file_list) == 0:
-        logger.info(f"No files to trim, exiting")
+        logger.info("No files to trim, exiting")
     else:
         for file_name in source_file_list:
             logger.info(f"Starting trimming sound for {file_name}")
 
-            tag = file_name.split('-',2)[1]
-            date = file_name.split(' ',2)[0]
+            tag = file_name.split("-", 2)[1]
+            date = file_name.split(" ", 2)[0]
 
-            df_active=df[df["tag"]==tag]
+            df_active = df[df["tag"] == tag]
             show_name, dj_name, ep_nr, genre = get_metadata(df_active)
             dst_audio_filename = get_filename(tag, show_name, dj_name, ep_nr, date)
 
-            dst_path = trim_sound(src_audio_path, file_name, dst_audio_path, dst_audio_filename)
+            dst_path = trim_sound(
+                src_audio_path, file_name, dst_audio_path, dst_audio_filename
+            )
 
-            date_rec = datetime.strptime(dst_audio_filename.split('_',2)[0], '%Y%m%d')
-            add_metadata_to_mp3(dst_path, show_name, dj_name, ep_nr, genre, date, date_rec.year)
+            date_rec = datetime.strptime(dst_audio_filename.split("_", 2)[0], "%Y%m%d") #noqa: DTZ007
+            add_metadata_to_mp3(
+                dst_path, show_name, dj_name, ep_nr, genre, date, date_rec.year
+            )
 
         logger.info("Finished trimming stage")
+
 
 def get_sheet(cred_path: str) -> pd.DataFrame:
     gc = gspread.service_account(filename=cred_path)
     spreadsheet = gc.open_by_key(SHEET_ID)
     worksheet = spreadsheet.worksheet(SHEET_NAME)
     rows = worksheet.get_all_records()
-    df = pd.DataFrame.from_dict(rows)
 
-    return df
+    return pd.DataFrame.from_dict(rows)
 
-def main():
-    parser = argparse.ArgumentParser(description='Program to trim dead audio and add fades')
-    parser.add_argument(
-        '--path',
-        default = "/var/lib/robot",
-        help = "Path to local root folder used for storing audio"
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Program to trim dead audio and add fades"
     )
     parser.add_argument(
-        '--credentials',
-        default = "/etc/robot/cred.json",
-        help = "Path to credentials file"
+        "--path",
+        default="/var/lib/robot",
+        help="Path to local root folder used for storing audio",
+    )
+    parser.add_argument(
+        "--credentials", default="/etc/robot/cred.json", help="Path to credentials file"
     )
 
     args = parser.parse_args()
-    df = get_sheet(args.credentials)
-    src_audio_path = Path(args.path) / "to_mix" 
-    dst_audio_path = Path(args.path) / "to_upload" 
+    df_data = get_sheet(args.credentials)
+    src_audio_path = Path(args.path) / "to_mix"
+    dst_audio_path = Path(args.path) / "to_upload"
     dst_audio_path.mkdir(parents=True, exist_ok=True)
 
-    trimming(df, str(src_audio_path), str(dst_audio_path))
+    trimming(df_data, str(src_audio_path), str(dst_audio_path))
+
 
 if __name__ == "__main__":
     main()
--- src/mixing/utils.py
+++ src/mixing/utils.py
@@ -1,23 +1,39 @@
 import eyed3
+import pandas as pd
 
-def get_metadata(metadata_df):
-    show_data=metadata_df[['show_name','dj_name','show_nr','tags-0-tag']].dropna(axis=1, how='all').to_dict('records')
+
+def get_metadata(metadata_df: pd.DataFrame) -> tuple(str, str, str, str):
+    show_data = (
+        metadata_df[["show_name", "dj_name", "show_nr", "tags-0-tag"]]
+        .dropna(axis=1, how="all")
+        .to_dict("records")
+    )
     show_data = show_data[0]
-    show_name = show_data['show_name']
-    dj_name = show_data['dj_name']
-    ep_nr = show_data['show_nr']
-    genre = show_data['tags-0-tag']
+    show_name = show_data["show_name"]
+    dj_name = show_data["dj_name"]
+    ep_nr = show_data["show_nr"]
+    genre = show_data["tags-0-tag"]
     return show_name, dj_name, ep_nr, genre
 
-def get_filename(tag, show_name, dj_name, ep_nr, date):
-    filename=f"{date}_{tag}_{show_name}_{ep_nr}_{dj_name}.mp3"
+
+def get_filename(tag: str, show_name: str, dj_name: str, ep_nr: str, date: str) -> str:
+    filename = f"{date}_{tag}_{show_name}_{ep_nr}_{dj_name}.mp3"
     return filename.replace(" ", "_").replace("/", "")
 
-def add_metadata_to_mp3(file_path, show_name, dj_name, ep_nr, genre, date, year):
+
+def add_metadata_to_mp3(
+    file_path: str,
+    show_name: str,
+    dj_name: str,
+    ep_nr: int,
+    genre: str,
+    date: str,
+    year: int,
+) -> None:
     # Load the MP3 file
     audiofile = eyed3.load(file_path)
 
-    if dj_name == "" or dj_name == None:
+    if dj_name == "" or dj_name is None:
         dj_name = show_name
 
     # Set metadata
@@ -27,7 +43,6 @@ def add_metadata_to_mp3(file_path, show_name, dj_name, ep_nr, genre, date, year)
     audiofile.tag.release_date = year
     audiofile.tag.track_num = (ep_nr, None)  # (track_number, total_tracks)
     audiofile.tag.genre = genre
-    
+
     # Save changes
     audiofile.tag.save()
-
